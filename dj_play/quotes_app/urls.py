823371cffcf33985e6c9dc7ef6de456e410ae52d
bcerney <brianecerney@gmail.com>
2020-10-01 09:25:17 -0400

    Part 2 of DRF tutorial: https://www.django-rest-framework.org/tutorial/2-requests-and-responses/

--- dj_play/quotes_app/urls.py
+++ dj_play/quotes_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from quotes_app import views
 
 urlpatterns = [
     path('quotes/', views.quote_list),
     path('quotes/<int:pk>/', views.quote_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- dj_play/quotes_app/views.py
+++ dj_play/quotes_app/views.py
@@ -1,52 +1,49 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from quotes_app.models import Quote
 from quotes_app.serializers import QuoteSerializer
 
 
-@csrf_exempt
-def quote_list(request):
+@api_view(['GET', 'POST'])
+def quote_list(request, format=None):
     """
-    List all code snippets, or create a new snippet.
+    List all quotes, or create a new quote.
     """
-    import pdb; pdb.set_trace()
     if request.method == 'GET':
         quotes = Quote.objects.all()
         serializer = QuoteSerializer(quotes, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = QuoteSerializer(data=data)
+        serializer = QuoteSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
-def quote_detail(request, pk):
+@api_view(['GET', 'PUT', 'DELETE'])
+def quote_detail(request, pk, format=None):
     """
-    Retrieve, update or delete a code snippet.
+    Retrieve, update or delete a quote.
     """
     try:
         quote = Quote.objects.get(pk=pk)
-    except Snippet.DoesNotExist:
-        return HttpResponse(status=404)
+    except Quote.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = QuoteSerializer(quote)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = QuoteSerializer(quote, data=data)
+        serializer = QuoteSerializer(quote, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         quote.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
