e3d371e70c398bac25a2d3112d9a6ef30ca18d53
bcerney <brianecerney@gmail.com>
2020-10-01 10:24:15 -0400

    Part 3 of DRF tutorial: https://www.django-rest-framework.org/tutorial/3-class-based-views/

--- dj_play/quotes_app/urls.py
+++ dj_play/quotes_app/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from quotes_app import views
 
 urlpatterns = [
-    path('quotes/', views.quote_list),
-    path('quotes/<int:pk>/', views.quote_detail),
+    path('quotes/', views.QuoteList.as_view()),
+    path('quotes/<int:pk>/', views.QuoteDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- dj_play/quotes_app/views.py
+++ dj_play/quotes_app/views.py
@@ -1,49 +1,57 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework import generics
 
 from quotes_app.models import Quote
 from quotes_app.serializers import QuoteSerializer
 
 
-@api_view(['GET', 'POST'])
-def quote_list(request, format=None):
-    """
-    List all quotes, or create a new quote.
-    """
-    if request.method == 'GET':
-        quotes = Quote.objects.all()
-        serializer = QuoteSerializer(quotes, many=True)
-        return Response(serializer.data)
+class QuoteList(generics.ListCreateAPIView):
+    queryset = Quote.objects.all()
+    serializer_class = QuoteSerializer
 
-    elif request.method == 'POST':
-        serializer = QuoteSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def quote_detail(request, pk, format=None):
-    """
-    Retrieve, update or delete a quote.
-    """
-    try:
-        quote = Quote.objects.get(pk=pk)
-    except Quote.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+class QuoteDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Quote.objects.all()
+    serializer_class = QuoteSerializer
 
-    if request.method == 'GET':
-        serializer = QuoteSerializer(quote)
-        return Response(serializer.data)
 
-    elif request.method == 'PUT':
-        serializer = QuoteSerializer(quote, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# @api_view(['GET', 'POST'])
+# def quote_list(request, format=None):
+#     """
+#     List all quotes, or create a new quote.
+#     """
+#     if request.method == 'GET':
+#         quotes = Quote.objects.all()
+#         serializer = QuoteSerializer(quotes, many=True)
+#         return Response(serializer.data)
 
-    elif request.method == 'DELETE':
-        quote.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+#     elif request.method == 'POST':
+#         serializer = QuoteSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def quote_detail(request, pk, format=None):
+#     """
+#     Retrieve, update or delete a quote.
+#     """
+#     try:
+#         quote = Quote.objects.get(pk=pk)
+#     except Quote.DoesNotExist:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
+
+#     if request.method == 'GET':
+#         serializer = QuoteSerializer(quote)
+#         return Response(serializer.data)
+
+#     elif request.method == 'PUT':
+#         serializer = QuoteSerializer(quote, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     elif request.method == 'DELETE':
+#         quote.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
