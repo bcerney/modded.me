db538adb229ed6b976564777d76fa706fcaa80f0
bcerney <brianecerney@gmail.com>
2020-10-12 11:08:36 -0400

    FIXME busted nginx codedump

--- dj_play/dj_play/settings.py
+++ dj_play/dj_play/settings.py
@@ -10,10 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 import environ
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -150,6 +150,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # DRF
 REST_FRAMEWORK = {
