b36bdbaf8ac52050088d23dcd9e76d0789c05b15
bcerney <brianecerney@gmail.com>
2020-10-24 13:44:45 -0400

    Initial Virtue model work

--- dj_play/dashboard/admin.py
+++ dj_play/dashboard/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import CustomUser
+from .models import CustomUser, Task, Topic, UserProfile, Virtue
 
 admin.site.register(CustomUser, UserAdmin)
+
+# class UserProfileAdmin(admin.ModelAdmin):
+#     list_display = ['id', 'user', 'virtues']
+
+# admin.site.register(UserProfile, UserProfileAdmin)
+
+models = UserProfile, Task, Topic, Virtue
+admin.site.register(models)
