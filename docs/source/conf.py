704c450abcaf37bc88fe7d8e81d97867bf67fca7
bcerney <brianecerney@gmail.com>
2020-10-29 09:24:17 -0400

    Add link to Docs in navbar, regenerate docs after a few small updates

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,13 +10,8 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-import django
-sys.path.insert(0, os.path.abspath('..'))
-os.environ["DJANGO_SETTINGS_MODULE"] = "dj_play.settings"
-django.setup()
-
+# import os
+# import sys
 
 # -- Project information -----------------------------------------------------
 
